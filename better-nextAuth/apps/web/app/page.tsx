3e8cc1aa4ec71357448e4ac53c9aa64bd81166ee
Jyotishman Das <jyotishmand78@gmail.com>
2025-10-18 00:58:46 +0530

    initialize better-nextAuth monorepo

---
+++ better-nextAuth/apps/web/app/page.tsx
@@ -0,0 +1,9 @@
+import { Button } from "@workspace/ui/components/button"
+
+export default function Page() {
+  return (
+    <div className="flex items-center justify-center min-h-screen">
+      <Button>Click</Button>
+    </div>
+  )
+}
