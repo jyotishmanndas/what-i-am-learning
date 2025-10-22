fece224f26f6944c4426306acfd4eff6ecbc969a
Jyotishman Das <jyotishmand78@gmail.com>
2025-10-22 17:10:46 +0530

    feat: add scrolling text animation

---
+++ Gsap/07_scrolling_text_animation/script.js
@@ -0,0 +1,26 @@
+window.addEventListener("wheel", (e)=>{
+    if(e.deltaY > 0){
+        gsap.to(".marque", {
+            transform: "translateX(-200%)",
+            delay: 1,
+            duration: 4,
+            repeat: -1,
+            ease: "none"
+        });
+
+        gsap.to(".marque img", {
+            rotate: 180
+        })
+    } else {
+        gsap.to(".marque", {
+            transform: "translateX(0%)",
+            delay: 1,
+            duration: 4,
+            repeat: -1,
+            ease: "none"
+        });
+        gsap.to(".marque img", {
+            rotate: 0
+        })
+    }
+})
